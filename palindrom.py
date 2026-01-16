95cf171e08c7dc4325c6e7ee3e7528e5fd7d8e63
Ananya jantli <jantliananya@gmail.com>
2026-01-16 09:39:47 +0530

    Update palindrom.py

--- palindrom.py
+++ palindrom.py
@@ -1,2 +1,10 @@
 def is_palindrome(text):
     return text == text[::-1]
+
+if __name__ == "__main__":
+    text = input("Enter a text: ")
+
+    if is_palindrome(text):
+        print("Yes, it is palindrome")
+    else:
+        print("No, it is not palindrome")
