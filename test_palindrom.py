0f2d4e32a44d583bd832e1ca79d95f7e37e2b4e6
Ananya jantli <jantliananya@gmail.com>
2026-01-16 09:57:33 +0530

    Update test_palindrom.py

--- test_palindrom.py
+++ test_palindrom.py
@@ -3,12 +3,12 @@ from palindrom import is_palindrom
 
 
 def test_palindrome_true():
-    assert is_palindrom("madam") == True
+    assert is_palindrome("madam") == True
 
 
 def test_palindrome_false_1():
-    assert is_palindrom("annu") == False
+    assert is_palindrome("annu") == False
 
 
 def test_palindrome_false_2():
-    assert is_palindrom("week") == False
+    assert is_palindrome("week") == False
